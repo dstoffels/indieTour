5150c38680dfb817833d722324e8a3d218ed6668
dstoffels <dan.stoffels@gmail.com>
2022-02-12 16:29:11 -0600

    added bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+const App = () => (
+	<div>
+		<h1>indieTour</h1>
+		<table className='table'>
+			<tbody>
+				<tr>
+					<td>Date</td>
+					<td>Location</td>
+				</tr>
+				<tr>
+					<td>Date</td>
+					<td>Location</td>
+				</tr>
+				<tr>
+					<td>Date</td>
+					<td>Location</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+);
+
+export default App;
