9ca8d1e1cf6959cc0a5e57a10e55b24024f9def7
dstoffels <dan.stoffels@gmail.com>
2022-03-12 09:46:14 -0600

    refactor: auth listener now updates userdata in db & store

--- src/Components/Auth/Provider/AuthProvider.jsx
+++ src/Components/Auth/Provider/AuthProvider.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { USER_PATH } from 'constants/restPaths.js';
 import { auth } from 'fb/firebase.js';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect } from 'react';
@@ -8,12 +10,21 @@ const AuthProvider = ({ children }) => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, user => {
-			user && dispatch(setUser(user.toJSON()));
-			!user && dispatch(setUser(null));
+		const unsubscribe = onAuthStateChanged(auth, async user => {
+			if (user) {
+				const { email, displayName, emailVerified } = user;
+				const updatedUser = await axios.put(
+					USER_PATH,
+					{ email, displayName, emailVerified },
+					{ headers: { auth: await user.getIdToken() } },
+				);
+				dispatch(setUser(updatedUser.data));
+			} else {
+				dispatch(setUser(null));
+			}
 		});
 		return () => unsubscribe();
-	}, [dispatch]);
+	}, []);
 
 	return children;
 };
--- src/constants/restPaths.js
+++ src/constants/restPaths.js
@@ -1,4 +1,5 @@
 const ROOT = 'http://127.0.0.1:3002';
 
+export const USER_PATH = ROOT + '/user';
 // BANDS
 export const BANDS_PATH = ROOT + '/bands';
